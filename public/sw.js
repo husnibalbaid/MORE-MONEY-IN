5fae995d03cd345d004193e113bd1c0025bc7ce8
husnibalbaid <husnibalbaid@gmail.com>
2026-07-21 19:38:38 +0700

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = "keuangan-v1";
+const urlsToCache = ["/", "/static/js/main.chunk.js", "/static/css/main.chunk.css"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache).catch(() => {}))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
